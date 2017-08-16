a7c9f98cfe5aa2c445ea9832ad8e625fb9cbe119
Reiner Jung <reiner.jung@email.uni-kiel.de>
2017-08-16 09:24:17 +0200

    cleanup of execution script

--- execute-observation.sh
+++ execute-observation.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 BASE=$(cd "$(dirname "$0")"; pwd)/
+DATA_DIR="$BASE/data"
+ANALYSIS_DIR="$DATA/analysis"
 
-rm -rf $BASE/data/kieker-*
-rm -rf $BASE/analysis/*
+COLLECTOR="$BASE/../collector-0.0.2-SNAPSHOT/bin/collector"
+KIEKER="$BASE/../kieker-1.12/bin"
 
-$BASE/collector-0.0.2-SNAPSHOT/bin/collector -d $BASE/data -p 9876
+if [ ! -d "$DATA_DIR" ] ; then
+	mkdir "$DATA_DIR"
+fi
+if [ ! -d "$ANALYSIS_DIR" ] ; then
+	mkdir "$ANALYSIS_DIR"
+fi
 
-$BASE/kieker-1.12/bin/trace-analysis.sh -i $BASE/data/kieker-* --plot-Deployment-Component-Dependency-Graph --plot-Container-Dependency-Graph --plot-Assembly-Component-Dependency-Graph --plot-Aggregated-Deployment-Call-Tree -o analysis
+rm -rf $DATA_DIR/kieker-*
+rm -rf $ANALYSIS_DIR/*
 
-$BASE/kieker-1.12/bin/dotPic-fileConverter.sh $BASE/analysis svg pdf
+$COLLECTOR -d "$DATA_DIR" -p 9876 &
+
+
+
+"$KIEKER/trace-analysis.sh" -i "$DATA_DIR"/kieker-* --plot-Deployment-Component-Dependency-Graph --plot-Container-Dependency-Graph --plot-Assembly-Component-Dependency-Graph --plot-Aggregated-Deployment-Call-Tree -o analysis
+
+"$KIEKER/dotPic-fileConverter.sh" "$ANALYSIS_DIR" svg pdf
 
 # end
 
