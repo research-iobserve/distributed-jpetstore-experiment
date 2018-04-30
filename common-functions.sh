d393737e6317001f0f9f839cb083989d59bf1016
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-04-30 12:44:06 +0200

    fixed functions

--- common-functions.sh
+++ common-functions.sh
@@ -13,11 +13,11 @@ function checkExecutable() {
 # $1 = NAME, $2 = FILE
 function checkFile() {
 	if [ "$2" == "" ] ; then
-		echo "$NAME variable for file not set."
+		echo "$1 variable for file not set."
 		exit 1
 	fi
 	if [ ! -f "$2" ] ; then
-		echo "$NAME not found at: $2"
+		echo "$1 not found at: $2"
 		exit 1
 	fi
 }
@@ -25,11 +25,11 @@ function checkFile() {
 # $1 = NAME, $2 = FILE
 function checkDirectory() {
 	if [ "$2" == "" ] ; then
-		echo "$NAME direcotry variable not set."
+		echo "$1 directory variable not set."
 		exit 1
 	fi
 	if [ ! -d "$2" ] ; then
-		echo "$NAME: Data directory $2 does not exist."
+		echo "$1: Data directory $2 does not exist."
 		exit 1
 	fi
 }
