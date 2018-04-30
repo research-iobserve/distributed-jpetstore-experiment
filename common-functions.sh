0da27203a3cdc72314773460e3279e801b5284f4
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-04-30 12:37:48 +0200

    fixed configurations

---
+++ common-functions.sh
@@ -0,0 +1,35 @@
+# $1 = NAME, $2 = EXECUTABLE
+function checkExecutable() {
+	if [ "$2" == "" ] ; then
+		echo "$1 variable for executable not set."
+		exit 1
+	fi
+	if [ ! -x "$2" ] ; then
+		echo "$1 not found at: $2"
+		exit 1
+	fi
+}
+
+# $1 = NAME, $2 = FILE
+function checkFile() {
+	if [ "$2" == "" ] ; then
+		echo "$NAME variable for file not set."
+		exit 1
+	fi
+	if [ ! -f "$2" ] ; then
+		echo "$NAME not found at: $2"
+		exit 1
+	fi
+}
+
+# $1 = NAME, $2 = FILE
+function checkDirectory() {
+	if [ "$2" == "" ] ; then
+		echo "$NAME direcotry variable not set."
+		exit 1
+	fi
+	if [ ! -d "$2" ] ; then
+		echo "$NAME: Data directory $2 does not exist."
+		exit 1
+	fi
+}
