faaf6497050043790b0a1374f667283e7099adad
Reiner Jung <reiner.jung@email.uni-kiel.de>
2018-05-02 12:24:40 +0200

    adapted kube experiment to new yaml file

--- execute-integrated-kube-experiment.sh
+++ execute-integrated-kube-experiment.sh
@@ -93,12 +93,16 @@ sleep 10
 
 echo ">>>>>>>>>>> start jpetstore"
 
-for I in account-deployment.yaml catalog-deployment.yaml frontend-deployment.yaml order-deployment.yaml ; do
-        cat $KUBERNETES_DIR/$I | sed "s/%LOGGER%/$LOGGER/g" > start.yaml
-	kubectl create -f start.yaml
-done
+#for I in account-deployment.yaml catalog-deployment.yaml frontend-deployment.yaml order-deployment.yaml ; do
+#        cat $KUBERNETES_DIR/$I | sed "s/%LOGGER%/$LOGGER/g" > start.yaml
+#	kubectl create -f start.yaml
+#done
+cat $KUBERNETES_DIR/jpetstore.yamle | sed "s/%LOGGER%/$LOGGER/g" > start.yaml
+kubectl create -f start.yaml
 
-rm start.yaml
+#rm start.yaml
+
+exit 0
 
 FRONTEND=""
 while [ "$FRONTEND" == "" ] ; do
